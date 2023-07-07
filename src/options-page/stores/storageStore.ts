cf750f68308ff35fcc6b1a87d1b787c69ae91ec3
Tom <1029520042@qq.com>
2023-07-07 15:15:44 +0800

    fix: 该BUG会导致配置页面在配置时必定报错

--- src/options-page/stores/storageStore.ts
+++ src/options-page/stores/storageStore.ts
@@ -61,15 +61,6 @@ export const useStorageStore = create<State, [["zustand/immer", never]]>(
     ): void {
       set((state) => {
         const target = state[configName];
-        if (!Object.prototype.hasOwnProperty.call(target, name))
-          /**
-           * 在设计上传入的 configName 与 name 是一一对应的。
-           *  例如 configName 为 "wordConfig" 时， name 的类型应该是 keyof WordConfig
-           *   因此当相应的属性不存在时，表面代码中存在BUG需要进行修复。
-           */
-          throw new Error(
-            `传入的${configName},${name}属性并不一一对应,这是一个不符合设计的BUG.`
-          );
         //@ts-ignore 是能够正确配对的
         target[name] = value;
       });
